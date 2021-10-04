9fe01b7c473c6726e718754ff2ea5ad2172d15e3
Maksims Liskins <olainerul@inbox.ru>
2021-10-04 14:39:03 +0300

    small fix

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-// Route::get('/test', function (Request $request) {
-//     return "Hello";
-// });
 Route::get('documents', 'DocumentController@index');
 
 Route::post('documents/create', 'DocumentController@create');
